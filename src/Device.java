8b58e51083d6593d7643632f2d21f46edc1288e4
Bogdan Koteluk <bogdan.koteluk@gmail.com>
2019-07-16 14:22:51 +0200

    Device class was created (with getters, setters and constructors)

---
+++ src/Device.java
@@ -0,0 +1,53 @@
+public class Device {
+    //code, producer, type, price
+    private String code;
+    private String producer;
+    private String type;
+    private double price;
+
+    public Device() {
+    }
+
+    public Device(String code, String producer, String type) {
+        setCode(code);
+        setProducer(producer);
+        setType(type);
+    }
+
+    public Device(String code, String producer, String type,double price) {
+        this(code,producer,type);
+        setPrice(price);
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getProducer() {
+        return producer;
+    }
+
+    public void setProducer(String producer) {
+        this.producer = producer;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+}
